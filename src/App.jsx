1bb766bf800d364ad9f941bcc79608a8c2e7585f
ahmedsami019 <easami019192@gmail.com>
2025-12-01 18:52:36 +0600

    install react-router, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className='text-4xl'>Explore Email and password authentication</h2>
+    </>
+  )
+}
+
+export default App
