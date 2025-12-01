a225416f72143826914f7764e08aeac75179fabf
ahmedsami019 <easami019192@gmail.com>
2025-12-01 19:43:28 +0600

    added event Handler for register

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,87 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
-    return (
-        <div>
-            <h2>this is register page</h2>
-        </div>
-    );
+
+    const handleRegister = e => {
+        e.preventDefault()
+        const email = e.target.email.value
+        const passowrd = e.target.password.value
+        console.log(email, passowrd);
+
+    }
+
+  return (
+    <div className="w-11/12 mx-auto text-center space-y-5">
+      <h2>this is register page</h2>
+
+      <form onSubmit={handleRegister} className="w-3/12 mx-auto flex flex-col border border-gray-300 p-10 rounded-2xl bg-gray-800">
+        {/* this is email field */}
+        <label className="input validator w-full">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+            </g>
+          </svg>
+          <input type="email" name="email" placeholder="mail@site.com" required />
+        </label>
+        <div className="validator-hint hidden">Enter valid email address</div>
+
+        <br />
+
+        {/* this is password field */}
+        <label className="input validator w-full">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+              <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+            </g>
+          </svg>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Password"
+            minlength="8"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+          />
+        </label>
+        <p className="validator-hint hidden">
+          Must be more than 8 characters, including
+          <br />
+          At least one number <br />
+          At least one lowercase letter <br />
+          At least one uppercase letter
+        </p>
+
+        <br />
+
+        <input className="btn btn-primary" type="submit" value="Sign up" />
+    
+      </form>
+    </div>
+  );
 };
 
 export default Register;
